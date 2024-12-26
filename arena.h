0eb6f14ea278b8844f51fdeb1f00fa7bd88b73c1
Pranith Koppula <koppularaghavendra@gmail.com>
2024-12-26 00:28:11 -0500

    indented class objects in arena.h

--- arena.h
+++ arena.h
@@ -5,16 +5,16 @@
 namespace arena {
 template <typename T>
 class Arena {
-public:
-    Arena(size_t bytes);
-    absl::StatusOr<T*> Allocate(T val);
+    public:
+        Arena(size_t bytes);
+        absl::StatusOr<T*> Allocate(T val);
 
-    size_t GetCurrentAllocation();
-    size_t GetTypeSize();
-private:
-    T* bytes_;
-    size_t type_size_;
-    size_t next_addr_;
-    size_t total_bytes_;
+        size_t GetCurrentAllocation();
+        size_t GetTypeSize();
+    private:
+        T* bytes_;
+        size_t type_size_;
+        size_t next_addr_;
+        size_t total_bytes_;
 };
-}
+} // namespace arena
