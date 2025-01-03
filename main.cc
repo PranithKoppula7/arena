b4425838424053d27650bea3bb3a5d2a16944117
Pranith Koppula <koppularaghavendra@gmail.com>
2025-01-03 00:09:07 -0500

    refactor allocateOneInteger test with expect

--- main.cc
+++ main.cc
@@ -3,13 +3,6 @@
 
 #include "arena.h"
 
-static void test(benchmark::State& state) {
-    int j = 0;
-    for(int i = 0; i < 1000000000; i++) {
-        j != i;
-    }
-}
-
 static void arena_initialization(benchmark::State& state) {
     arena::Arena<int> a = arena::Arena<int>(8);
 }
