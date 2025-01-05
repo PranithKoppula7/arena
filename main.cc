56a0098068031b090d3b4599deeb08cdbcfdf055
Pranith Koppula <koppularaghavendra@gmail.com>
2025-01-05 15:36:45 -0500

    trying to get memory usage working; not showing up yet

--- main.cc
+++ main.cc
@@ -4,6 +4,35 @@
 
 #include "arena.h"
 
+class SimpleMemoryManager : public benchmark::MemoryManager {
+public:
+    int64_t num_allocs;
+    int64_t max_bytes_used;
+
+
+    void Start() BENCHMARK_OVERRIDE {
+        num_allocs = 0;
+        max_bytes_used = 0;
+    }
+
+    void Stop(Result& result) BENCHMARK_OVERRIDE {
+        result.num_allocs = num_allocs;
+        result.max_bytes_used = max_bytes_used;
+    }
+};
+
+std::unique_ptr<SimpleMemoryManager> mm(new SimpleMemoryManager());
+
+// #ifdef MEMORY_PROFILER
+void *custom_malloc(size_t size) {
+    void *p = malloc(size);
+    mm.get()->num_allocs += 1;
+    mm.get()->max_bytes_used += size;
+    return p;
+}
+#define malloc(size) custom_malloc(size)
+// #endif
+
 static void arena_initialization(benchmark::State& state) {
     for (auto _ : state) {
         arena::Arena<int> a = arena::Arena<int>(8);
@@ -28,12 +57,21 @@ static void long_loop_test(benchmark::State& state) {
     }
 }
 
+static void BM_memory(benchmark::State& state) {
+    for (auto _ : state)
+        for (int i =0; i < 10; i++) {
+            benchmark::DoNotOptimize((int *) malloc(10 * sizeof(int *)));
+        }
+}
+
+BENCHMARK(BM_memory)->Unit(benchmark::kMillisecond)->Iterations(17);
+
 
 BENCHMARK(arena_initialization);
 BENCHMARK(manual_loop_mallocation);
 
 int main(int argc, char** argv) {
-    ::benchmark::RegisterMemoryManager(0);
+    ::benchmark::RegisterMemoryManager(mm.get());
     ::benchmark::Initialize(&argc, argv);
     ::benchmark::RunSpecifiedBenchmarks();
     ::benchmark::RegisterMemoryManager(nullptr);
